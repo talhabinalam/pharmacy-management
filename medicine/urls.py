cae2433b2337dae16c7d25e0aacb98931cc120ba
talhabinalam <mdtalhabinalam@outlook.com>
2024-10-28 05:45:55 +0600

    added login and logout functionality

--- medicine/urls.py
+++ medicine/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('search/', SearchView.as_view(), name='search'),
     path('all_medicines/', AllMedicinesView.as_view(), name='all_medicines'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', login_required(LogoutView.as_view()), name='logout'),
 ]
--- medicine/views.py
+++ medicine/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from medicine.models import Medicine
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class IndexView(View):
     def get(self, request):
         medicines = Medicine.objects.all()
         paginator = Paginator(medicines, 5)  # Show 5 contacts per page.
 
+        user = request.user
+
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
         context = {
-            'page_obj': page_obj
+            'page_obj': page_obj,
+            'user': user,
                    }
         return render(request, 'index.html', context)
 
@@ -45,3 +52,34 @@ class AllMedicinesView(View):
     def get(self, request):
         medicines = Medicine.objects.all()
         return render(request, 'all_medicines.html', {'medicines': medicines})
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None and user.is_superuser:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'You must have superuser access!')
+            return redirect('login')
+
+
+class LogoutView(View):
+    login_url = '/login/'
+    def post(self, request):
+        logout(request)
+        return redirect('index')
+
+
+class AddMedicineView(View):
+    def post(self, request):
+        if request.user.is_authenticated:
+            medicine_name = request.POST.get('medicine_name')
