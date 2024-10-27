90d0c218ca2980f49c95a2acf6c4ed5b0eb291d8
talhabinalam <mdtalhabinalam@outlook.com>
2024-10-28 01:33:50 +0600

    listing and search functionality completed

--- medicine/urls.py
+++ medicine/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('search/', SearchView.as_view(), name='search'),
+    path('all_medicines/', AllMedicinesView.as_view(), name='all_medicines'),
 ]
--- medicine/views.py
+++ medicine/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from medicine.models import Medicine
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 class IndexView(View):
     def get(self, request):
@@ -14,3 +15,33 @@ class IndexView(View):
             'page_obj': page_obj
                    }
         return render(request, 'index.html', context)
+
+
+class SearchView(View):
+    def get(self, request):
+        query = request.GET.get('q')  # Get the search query from the request
+        medicines = Medicine.objects.all()
+
+        if query:  # Check if there is a search keyword
+            # Use Q to filter based on case-insensitive search for name or generic name
+            medicines = medicines.filter(
+                Q(name__icontains=query) |
+                Q(generic_name__icontains=query)
+            )
+            # print(f"Query: {query}, Results found: {medicines.count()}")
+
+        else:
+            medicines = Medicine.objects.none()
+            print("No search query provided.")
+
+        context = {
+            'medicines': medicines,
+            'query': query
+        }
+        return render(request, 'search_results.html', context)
+
+
+class AllMedicinesView(View):
+    def get(self, request):
+        medicines = Medicine.objects.all()
+        return render(request, 'all_medicines.html', {'medicines': medicines})
